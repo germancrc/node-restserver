3105ed508e82abae0e1a0f33f9326120ec2167f8
germancrc <german.cruz36@gmail.com>
2020-10-11 19:12:32 -0400

    Primer commit

---
+++ server/server.js
@@ -0,0 +1,49 @@
+require('./config/config');
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser')
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+ 
+// Peticiones HTTP
+app.get('/usuario', function (req, res) {
+  res.json('Usuario obtenido')
+});
+
+app.post('/usuario', function (req, res) {
+    let body = req.body;
+
+    if ( body.nombre === undefined ) {
+        res.status(400).json({
+            ok: false,
+            mensaje: 'Es necesario poner el nombre'
+        });
+    } else {
+        res.json({
+            usuario: body
+        });
+    }
+
+});
+
+app.put('/usuario/:id', function (req, res) {
+    let id = req.params.id;
+    res.json({
+        id
+    });
+});
+
+app.delete('/usuario', function (req, res) {
+    res.json('Usuario Borrado')
+});
+ 
+
+// Listening
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando puerto', 3000);
+});
