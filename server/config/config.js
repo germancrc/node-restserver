f2efdbb963464056c583bcdfab0ca5716aff476e
germancrc <german.cruz36@gmail.com>
2020-10-13 16:39:54 -0400

    Variable de entorno MongoDB

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if ( process.env.NODE_ENV === 'dev' ){
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://germancrc:zpL49iRbGtu316iZ@cluster0.mis01.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
